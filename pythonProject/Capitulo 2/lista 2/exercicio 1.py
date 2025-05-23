25571c3b619be11c8488ae918f642d0eb4cf12c2
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-23 11:56:42 -0300

    exercicio2

---
+++ pythonProject/Capitulo 2/lista 2/exercicio 1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+
+soma = num1 + num2
+produto = num1 * num2
+diferenca = num1 - num2
+quociente = num1 / num2
+
+print ("A soma é: ", soma)
+print ("O produto é: ", produto)
+print ("A diferença é: ", diferenca)
+print ("O quociente é: ", quociente)
---
+++ pythonProject/Capitulo 2/lista 2/exercicio2.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um número de 5 digitos: "))
+
+numero1 = (numero // 10000) % 10
+numero2 = (numero // 1000) % 10
+numero3 = (numero // 100) % 10
+numero4 = (numero // 10) % 10
+numero5 = numero % 10
+
+print(digito1, " ", numero1)
+print(digito2, =)
